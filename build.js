65a89d644885d2d01f043aaaf0cf930c6642987f
gasolin <gasolin@gmail.com>
2015-11-25 16:02:05 +0800

    foreach works

--- build.js
+++ build.js
@@ -3,7 +3,22 @@ var handle = require('preprocess');
 var src = 'index.html';
 var dest = 'dist/index.html';
 
-handle.preprocessFile(src, dest, {}, function() {console.log('done')},
+handle.preprocessFile(src, dest, {
+  PANELS: [
+    'wifi-manageNetworks',
+    'wifi-manageCertificates',
+    'wifi-selectCertificateFile',
+    'wifi'
+  ].toString(),
+  DIALOGS: [
+    'wifi-status',
+    'wifi-auth',
+    'wifi-joinHidden',
+    'wifi-wps',
+    'wifi-enterCertificateNickname'
+  ].toString()
+},
+  function() {console.log('done')},
 {
   srcDir: './',
   srcEol: '\r\n'
